cf48dc5e958abfce225f034a7f4e2dd303dcff7c
AmandaMitic <am3646mi-s@student.lu.se>
2021-03-30 20:00:07 +0200

    Added a compass picture.

--- app/src/main/java/com/example/hellosensors/CompassActivity.java
+++ app/src/main/java/com/example/hellosensors/CompassActivity.java
@@ -2,13 +2,51 @@ package com.example.hellosensors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
-public class CompassActivity extends AppCompatActivity {
+public class CompassActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensormanager;
+    private Sensor vector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+
+        sensormanager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        vector = sensormanager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //KOD
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //TOM
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensormanager.registerListener(this, vector, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensormanager.unregisterListener(this);
     }
 }
